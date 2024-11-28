75c36bfef60376401cde02d68811bdc34aef3264
danishubin <dshubin@eastsideprep.org>
2024-11-27 22:28:28 -0800

    login page

--- script/login.js
+++ script/login.js
@@ -14,3 +14,25 @@ document.getElementById('codeButton').onclick = function(){
       warning.style.visibility = "visible";
     }
   };
+
+
+  // Get the input field
+var input = document.getElementById("code");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    if ((code.value).toLowerCase() == "vitamason") { // No Rehearsal No +1
+      window.location.href = "index.html?code=vitamason";  // Replace with your custom page URL
+    } else if ((code.value).toLowerCase() == "vitamason-rd") { // Rehearsal No +1
+      window.location.href = "index.html?code=vitamason-rd";  // Replace with your custom page URL
+    } else if ((code.value).toLowerCase()== "vitamason-1") { // No Rehearsal + 1
+      window.location.href = "index.html?code=vitamason-1";  // Replace with your custom page URL
+    } else if ((code.value).toLowerCase()== "vitamason-rd1") { // Rehearsal + 1
+      window.location.href = "index.html?code=vitamason-rd1";  // Replace with your custom page URL
+    } else {
+      warning.style.visibility = "visible";
+    }
+  }
+});
